35dc440ceb7098c3653576fb67b3409c21585b49
tekku-taro <tekku.taro@gmail.com>
2025-03-11 17:31:04 +0900

    update auth.actions.ts

--- app/actions/auth.actions.ts
+++ app/actions/auth.actions.ts
@@ -1,5 +1,5 @@
 "use server"
-import bcrypt, { hash } from "bcryptjs"
+import bcrypt from "bcryptjs"
 import { prisma } from "@/prisma"
 import { v4 as uuidv4 } from "uuid"
 import { sendPasswordResetEmail, sendVerificationEmail } from "@/lib/email"
@@ -7,6 +7,8 @@ import { z } from "zod"
 import { signIn, signOut } from "@/auth"
 import { isRedirectError } from "next/dist/client/components/redirect-error"
 import { redirect } from "next/navigation"
+import appConfig from "@/lib/config"
+import { generateVerificationCode } from "@/lib/utils"
 
 // バリデーションスキーマ
 const loginSchema = z.object({
@@ -23,6 +25,29 @@ const registerSchema = z.object({
   message: "Passwords don't match",
   path: ['confirmPassword'],
 })
+
+const verifyEmailSchema = z.object({
+  email: z.string().email({ message: "Please enter a valid email address" }),
+  code: z.string().length(6, { message: "Verification code must be 6 characters" }),
+})
+
+const NewPasswordSchema = z.object({
+  token: z.string().min(1, {
+    message: "Token is required",
+  }),
+  password: z.string().min(6, { message: "Password must be at least 6 characters" }).trim(),
+  confirmPassword: z.string().min(6, 'Confirm password must be at least 6 characters').trim(),
+}).refine((data) => data.password === data.confirmPassword, {
+  message: "Passwords don't match",
+  path: ['confirmPassword'],
+})
+
+const ResetPasswordSchema = z.object({
+  email: z.string().email({
+    message: "Email is required",
+  }),
+});
+
 export type FormState =
   {
       status?: string;  
@@ -91,44 +116,51 @@ export async function registerUser(prevState:FormState, formData:FormData) {
       },
     })
 
-    await signIn('credentials', {
-      email: user.email,
-      password: password,
-    })
+    if (appConfig.emailVerificationRequired) {
+      // Generate verification token
+      const token = generateVerificationCode()
+      const expires = new Date(Date.now() + 1 * 60 * 60 * 1000) // 1 hours
+  
+      await prisma.verificationToken.create({
+        data: {
+          identifier: email,
+          token,
+          expires,
+        },
+      })
+  
+      // Send verification email
+      await sendVerificationEmail({
+        email,
+        token,
+        name: name || "",
+      })
 
-    return {
-      status: "success", 
-      message: 'User registered successfully',
-      callbackUrl
-    }    
+      return {
+        status: "success", 
+        message: 'Please verify your email address before signing in',
+        callbackUrl:`/verify-email?email=${email}`
+      }
 
+    } else {
+      await signIn('credentials', {
+        email: user.email,
+        password: password,
+        redirect: false,
+      })
+
+      return {
+        status: "success", 
+        message: 'User registered successfully',
+        callbackUrl
+      }    
+    }
   } catch (error) {
     if (isRedirectError(error)) {
       throw error;
     }
-    return { status: 'error', message: "can't register user. Try again." };    
+    return { status: 'error', message: "Failed to register user. Try again." };    
   }
-
-
-
-  // Generate verification token
-  // const token = uuidv4()
-  // const expires = new Date(Date.now() + 24 * 60 * 60 * 1000) // 24 hours
-
-  // await prisma.verificationToken.create({
-  //   data: {
-  //     identifier: email,
-  //     token,
-  //     expires,
-  //   },
-  // })
-
-  // Send verification email
-  // await sendVerificationEmail({
-  //   email,
-  //   token,
-  //   name: name || "",
-  // })
 }
 
 
@@ -179,13 +211,26 @@ export async function loginUser(prevState:FormState, formData:FormData) {
 }
 
 // Verify email
-export async function verifyEmail({
-  email,
-  code,
-}: {
-  email: string
-  code: string
-}) {
+export async function verifyEmail(prevState:FormState, formData:FormData) {
+  
+  // バリデーション
+  const validatedFields = verifyEmailSchema.safeParse(
+    {
+      email: formData.get('email'),
+      code: formData.get('code'),
+    }
+  )
+  // If any form fields are invalid, return early
+  if (!validatedFields.success) {
+    return {
+      status: "error",
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Validation error. Try again."
+    }
+  }
+
+  const { code, email } = validatedFields.data
+
   // Find verification token
   const verificationToken = await prisma.verificationToken.findFirst({
     where: {
@@ -193,7 +238,6 @@ export async function verifyEmail({
       token: code,
     },
   })
-
   if (!verificationToken) {
     throw new Error("Invalid verification code")
   }
@@ -202,31 +246,55 @@ export async function verifyEmail({
     throw new Error("Verification code has expired")
   }
 
-  // Update user
-  await prisma.user.update({
-    where: {
-      email,
-    },
-    data: {
-      emailVerified: new Date(),
-    },
-  })
-
-  // Delete verification token
-  await prisma.verificationToken.delete({
-    where: {
-      identifier_token: {
-        identifier: email,
-        token: code,
+  try {  
+    // Update user
+    await prisma.user.update({
+      where: {
+        email,
       },
-    },
-  })
+      data: {
+        emailVerified: new Date(),
+      },
+    })
+  
+    // Delete verification token
+    await prisma.verificationToken.delete({
+      where: {
+        identifier_token: {
+          identifier: email,
+          token: code,
+        },
+      },
+    })    
 
-  return true
+    return {
+      status: "success", 
+      message: 'Your email has been verified. You can now sign in.',
+    }       
+  } catch (error) {
+    if (isRedirectError(error)) {
+      throw error;
+    }
+    return { status: 'error', message: "Failed to verify email. Try again." };    
+  }
 }
 
 // Reset password
-export async function resetPassword(email: string) {
+export async function resetPassword(prevState:FormState, formData:FormData) {
+  const validatedFields = ResetPasswordSchema.safeParse({
+    email: formData.get('email'),    
+  });
+
+  if (!validatedFields.success) {
+    return {
+      status: "error",
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Validation error. Try again."
+    }    
+  }
+
+  const { email } = validatedFields.data;
+
   // Check if user exists
   const user = await prisma.user.findUnique({
     where: {
@@ -242,46 +310,65 @@ export async function resetPassword(email: string) {
   const token = uuidv4()
   const expires = new Date(Date.now() + 1 * 60 * 60 * 1000) // 1 hour
 
-  // Delete any existing reset tokens for this user
-  await prisma.resetPasswordToken.deleteMany({
-    where: {
-      email,
-    },
-  })
+  try {
+    // Delete any existing reset tokens for this user
+    await prisma.resetPasswordToken.deleteMany({
+      where: {
+        email,
+      },
+    })
 
-  // Create new reset token
-  await prisma.resetPasswordToken.create({
-    data: {
+    // Create new reset token
+    await prisma.resetPasswordToken.create({
+      data: {
+        email,
+        token,
+        expires,
+      },
+    })
+
+    // Send password reset email
+    await sendPasswordResetEmail({
       email,
       token,
-      expires,
-    },
-  })
+      name: user.name || "",
+    })
+    
+  } catch (error) {
+    if (isRedirectError(error)) {
+      throw error;
+    }
+    return { status: 'error', message: "Failed to send password reset email. Please try again." };    
+  }  
 
-  // Send password reset email
-  await sendPasswordResetEmail({
-    email,
-    token,
-    name: user.name || "",
-  })
-
-  return true
+  return {
+    status: "success", 
+    message: 'Check your email for a link to reset your password',
+  }
 }
 
 // Update password with reset token
-export async function updatePasswordWithToken({
-  email,
-  token,
-  password,
-}: {
-  email: string
-  token: string
-  password: string
-}) {
+export async function updatePasswordWithToken(prevState:FormState, formData:FormData) {
+
+  const validatedFields = NewPasswordSchema.safeParse({
+    token: formData.get('token'),
+    password: formData.get('password'),
+    confirmPassword: formData.get('confirmPassword'),
+  });
+
+  if (!validatedFields.success) {
+    return {
+      status: "error",
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Validation error. Try again."
+    }  
+  }
+
+  const { token, password } = validatedFields.data;
+
   // Find reset token
-  const resetToken = await prisma.resetPasswordToken.findFirst({
+  const resetToken = await prisma.resetPasswordToken.findUnique({
     where: {
-      email,
       token,
     },
   })
@@ -294,27 +381,48 @@ export async function updatePasswordWithToken({
     throw new Error("Reset token has expired")
   }
 
-  // Hash new password
-  const hashedPassword = await hash(password, 10)
-
-  // Update user password
-  await prisma.user.update({
+  const user = await prisma.user.findUnique({
     where: {
-      email,
-    },
-    data: {
-      password: hashedPassword,
+      email: resetToken.email,
     },
   })
 
-  // Delete reset token
-  await prisma.resetPasswordToken.delete({
-    where: {
-      id: resetToken.id,
-    },
-  })
+  if (!user) {
+    throw new Error("User not found")
+  }
 
-  return true
+  try {
+    // Hash new password
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    // Update user password
+    await prisma.user.update({
+      where: {
+        id: user.id,
+      },
+      data: {
+        password: hashedPassword,
+      },
+    })
+
+    // Delete reset token
+    await prisma.resetPasswordToken.delete({
+      where: {
+        id: resetToken.id,
+      },
+    })    
+  } catch (error) {
+    if (isRedirectError(error)) {
+      throw error;
+    }
+    return { status: 'error', message: "Failed to update password. Please try again." };    
+  } 
+
+
+  return {
+    status: "success", 
+    message: 'Your password has been successfully updated.',
+  }
 }
 
 
